808192f8d6075017e5d94e611165a57082f098d2
masterUNG <phrombutr@gmail.com>
2015-03-11 00:34:21 +0700

    ยังมีบัค

--- app/src/main/java/appewtc/masterung/testmap/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/testmap/MapsActivity.java
@@ -1,7 +1,15 @@
 package appewtc.masterung.testmap;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,7 +21,11 @@ public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private double douLatEWTC = 13.667646, doulngEWTC = 100.621740;
-    private LatLng objLatLngBangna14, objLatLngBTS;
+    private LatLng objLatLngBangna14, objLatLngBTS, objLatLngMobile;
+
+    private double douLatMobile, douLngMobile;
+    private LocationManager objLocationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +36,108 @@ public class MapsActivity extends FragmentActivity {
         setupLatLng();
 
         setUpMapIfNeeded();
+
+        objLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        //Check Status
+        checkStatus();
+
     }   // onCreate
+
+    public Location requestProvider(final String strPrivider, String strError) {
+        Location objLocation = null;
+        if (objLocationManager.isProviderEnabled(strPrivider)) {
+            objLocationManager.requestLocationUpdates(strPrivider, 1000, 10, objLocationListener);
+            objLocation = objLocationManager.getLastKnownLocation(strPrivider);
+        } else {
+            Log.d("master", "Error ==> " + strError);
+        }
+        return objLocation;
+    }
+
+    public final LocationListener objLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            douLatMobile = location.getLatitude();
+            douLngMobile = location.getLongitude();
+            Log.d("master", "Lat == Lng" + Double.toString(douLngMobile));
+        }   // onLocationChange
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void checkStatus() {
+        if (objLocationManager == null) {
+            Toast.makeText(MapsActivity.this, "Device not Support Location", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }   // checkStatus
 
     private void setupLatLng() {
 
         objLatLngBangna14 = new LatLng(13.669442, 100.623291);
         objLatLngBTS = new LatLng(13.668212, 100.605009);
+        objLatLngMobile = new LatLng(douLatMobile, douLngMobile);
+
 
     }   // setupLatLng
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        boolean bolGPS = objLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean bolNetwork = objLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!bolGPS && !bolNetwork) {
+            Intent objIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            Log.d("master", "Cannot Connected");
+        } else {
+            Log.d("master", "Connected");
+        }
+
+    }   // onStart
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (objLocationManager != null) {
+            objLocationManager.removeUpdates(objLocationListener);
+        }
+    }   // onStop
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
-    }
+        setUp();
+    }   // onResume
+
+    public void setUp() {
+        objLocationManager.removeUpdates(objLocationListener);
+        Location networkLocation = requestProvider(LocationManager.NETWORK_PROVIDER, "Network Error");
+        if (networkLocation != null) {
+            douLatMobile = networkLocation.getLatitude();
+            douLngMobile = networkLocation.getLongitude();
+        }
+        Location gpsLocation = requestProvider(LocationManager.GPS_PROVIDER, "GPS Error");
+        if (gpsLocation != null) {
+            douLatMobile = gpsLocation.getLatitude();
+            douLngMobile = gpsLocation.getLongitude();
+        }
+    }   // setUp
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
@@ -62,7 +162,8 @@ public class MapsActivity extends FragmentActivity {
                     .getMap();
 
             //Move Camera to EWTC
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(douLatEWTC, doulngEWTC), 16));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(douLatEWTC, doulngEWTC), 15));
+            //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(douLatMobile, douLngMobile), 15));
 
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
@@ -85,6 +186,8 @@ public class MapsActivity extends FragmentActivity {
 
         // for BTS
         mMap.addMarker(new MarkerOptions().position(objLatLngBTS).title("BTS").snippet("กรมอุตุ"));
+
+        mMap.addMarker(new MarkerOptions().position(objLatLngMobile).title("Mobile"));
 
     }   // setUpMap
 
